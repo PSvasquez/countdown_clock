eb5c1cc783411e1ac866e90d635f277fed244059
PSVasquez <patriciasantana199922@gmail.com>
2016-08-05 11:45:23 -0400

    update

---
+++ functions.js
@@ -0,0 +1,32 @@
+var deadline = 'January 1 2017';
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  function updateClock(){
+  var t = getTimeRemaining(endtime);
+  clock.innerHTML = 'days: ' + t.days + '<br>' +
+                    'hours: '+ t.hours + '<br>' +
+                    'minutes: ' + t.minutes + '<br>' +
+                    'seconds: ' + t.seconds;
+  if(t.total<=0){
+    clearInterval(timeinterval);
+  }
+}
+
+updateClock();
+var timeinterval = setInterval(updateClock,1000);
+}
+initializeClock('clockdiv', deadline);
